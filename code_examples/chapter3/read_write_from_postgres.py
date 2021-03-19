add177ab9d47b0d7c4814c787ab060be7bbd1167
undersfx <atchin_@hotmail.com>
2021-03-19 15:03:11 -0300

    add: Write cursor to csv with psycopg2

--- code_examples/chapter3/read_write_from_postgres.py
+++ code_examples/chapter3/read_write_from_postgres.py
@@ -7,6 +7,10 @@ cursor = connection.cursor()
 query = "select * from users"
 cursor.execute(query)
 
+# Some attributes are set by psycopg
+print('Number of rows in cursor:', cursor.rowcount)
+print('Number that cursor is placed:', cursor.rownumber)
+
 # cur.fetchone() # Return the a list with next tuple from select response
 # cur.fetchmany(10) # Return a list with the next 10 tuples from select response
 # cur.fetchall() # Return a list with all tuples of data from select response
@@ -14,3 +18,7 @@ cursor.execute(query)
 # Alternative
 for record in cursor:
     print(record)
+
+# Write to a CSV
+with open('data.csv', 'w') as file:
+    cursor.copy_to(file, 'users', sep=',')
